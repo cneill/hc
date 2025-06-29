6c52fa92a9b6ac1c856fafaacbe8e39cc3972506
Charles Neill <1749665+cneill@users.noreply.github.com>
2025-06-28 20:30:39 -0500

    Adding some response options

--- response_opts.go
+++ response_opts.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"slices"
 )
 
 type ResponseOpt func(response *http.Response) (*http.Response, error)
@@ -26,6 +27,50 @@ func JSONResponse(holder any) ResponseOpt {
 		decoder := json.NewDecoder(response.Body)
 		if err := decoder.Decode(holder); err != nil {
 			return nil, fmt.Errorf("JSONResponse: failed to decode: %w", err)
+		}
+
+		return response, nil
+	}
+}
+
+func BodyRead(response *http.Response) (*http.Response, error) {
+	_, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body: %w", err)
+	}
+
+	return response, nil
+}
+
+var Status2XX = []int{
+	http.StatusOK,
+	http.StatusCreated,
+	http.StatusAccepted,
+	http.StatusNonAuthoritativeInfo,
+	http.StatusNoContent,
+	http.StatusResetContent,
+	http.StatusPartialContent,
+	http.StatusMultiStatus,
+	http.StatusAlreadyReported,
+	http.StatusIMUsed,
+}
+
+type StatusCodeError struct {
+	StatusCode int
+	Status     string
+}
+
+func (s StatusCodeError) Error() string {
+	return fmt.Sprintf("status code: %d, message: %q", s.StatusCode, s.Status)
+}
+
+func AllowedStatusCodes(statusCodes ...int) ResponseOpt {
+	return func(response *http.Response) (*http.Response, error) {
+		if !slices.Contains(statusCodes, response.StatusCode) {
+			return nil, StatusCodeError{
+				StatusCode: response.StatusCode,
+				Status:     response.Status,
+			}
 		}
 
 		return response, nil
